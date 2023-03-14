b388f6b65ff102bc1840da5e5ff1d55720f68a1c
Alex Salinero Negro <alexsalineronegro@gmail.com>
2023-03-14 09:43:17 +0100

    First commit: Application and LoginController scaffolds

---
+++ src/1dami/tartangamngmt/app/Application.java
@@ -0,0 +1,14 @@
+package 1dami.tartangamngmt.app;
+/**
+*main package for the application
+**/
+
+
+public class Application{
+
+	public satic void main(String args){
+	/**
+	*Entry point for the aplication
+	**/
+	}
+}
